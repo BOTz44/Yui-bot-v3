fc4937fa103e32e2443a04929d38efa97334e512
BOTz44 <106071317+BOTz44@users.noreply.github.com>
2022-10-06 11:43:47 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -18,10 +18,10 @@ let str = `${global.wm}
 let wibu = `https://i.ibb.co/P5BBzbf/286637238-392674839545088-948545114162206238-n.jpg` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Sewa Bot','.sewa', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/q.c.ditya",
+    mediaUrl: "https://www.instagram.com/angga_ozmy?r=nametag",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CZX2NcAgqmF/?igshid=YmMyMTA2M2Y=", 
-    title: 'DITYA BOTZ MultiDevice',
+    title: 'Yui Chan MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sig
