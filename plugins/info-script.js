1833eff34028a7b10c03048c053bbe8ecc410eee
BOTz44 <106071317+BOTz44@users.noreply.github.com>
2022-10-06 11:42:09 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -1,32 +1,25 @@
+//Credits Jangan Dihapus
+//Thanks For KannaChan & Papah-Chan
 import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `*https://youtube.com/channel/UCxkIPWS8gVxomQTVWxetdSg*
-
-Pengen SC nya ?
-Nih chat WA....
-https://wa.me/6285237596750
-
-Pengen yang udah di recode oleh owner?
-Script bot ini dijual bila kalian mau bisa chat owner
-
-wa.me/6285237596750`
-let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
+let str = `*https://dwiqi.my.id/root/Mizu*`
+let wibu = `https://api.zacros.my.id/randomimg/waifu` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/q.c.ditya",
+    mediaUrl: "https://www.instagram.com/angga_ozmy?r=nametag._",
     mediaType: "VIDEO",
-    description: "https://www.instagram.com/p/CZX2NcAgqmF/?igshid=YmMyMTA2M2Y=", 
-    title: 'DITYA BOTZ MultiDevice',
+    description: "https://www.instagram.com/reel/Ce_bngYBkEA/?igshid=YmMyMTA2M2Y=", 
+    title: 'Yui-Chan MultiDevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
   }
   } }) 
           }
-handler.help = ['source code']
-handler.tags = ['info']
+handler.help = ['sc', 'script']
+handler.tags = ['info', 'main']
 handler.command =  /^(script|sc)$/i
 
 export default handler
