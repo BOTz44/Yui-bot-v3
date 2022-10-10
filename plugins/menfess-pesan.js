0cbae7277f7f6b3d74dc29790175ef4ec9e02695
BOTz44 <106071317+BOTz44@users.noreply.github.com>
2022-10-10 16:19:12 +0700

    Update menfess-pesan.js

--- plugins/menfess-pesan.js
+++ plugins/menfess-pesan.js
@@ -1,48 +1,58 @@
-let handler = async(m, { conn, text, usedPrefix }) => {
-let [number, pesan] = text.split `|`
+import fetch from 'node-fetch'
+let handler = async(m, {
+	conn, text, usedPrefix, command, args
+}) => {
+	if (!args[0]) return conn.reply(m.chat, `Silahkan masukan pesannya\nContoh Penggunaan: ${usedPrefix + command} ${nomorown} pesan untuknya`, m)
+	let mention
+    if (m.isGroup) mention = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
+    else mention = m.sender
+	if (!mention) throw 'Tag salah satu lah'
+	let txt = (args.length > 1 ? args.slice(1).join(' ') : '') || ''
+	let q = m.quoted ? m.quoted : m
+	let mime = (q.msg || q).mimetype || ''
+	let pengirim = m.sender
+	let cap = `╭━━━━「 👋 Saya ${conn.user.name} Ada Pesan Dari seseorang Untuk Kamu 」
+┊
+┊👥 Sender : Private
+┊⏰ Jam : ${bottime}
+┊
+╰═┅═━––––––๑
 
-    if (!number) return conn.reply(m.chat, 'Maaf Format Anda Salah\n\nContoh:\n.menfess 62××××|hallo sayang', m)
-    if (!pesan) return conn.reply(m.chat, 'Maaf Format Anda Salah\n\nContoh:\n.menfess 62××××|hallo sayang', m)
-    if (text > 500) return conn.reply(m.chat, 'Teks Kepanjangan!', m)
-    
-    let user = global.db.data.users[m.sender]
 
-    let korban = `${number}`
-    var nomor = m.sender
-    let spam1 = `Hi Saya Bot Ada Yang Kirim Pesan Ke Kamu
-Seseorang Temanmu
-(Pengirim Rahasia)
-⬡──⬡─────────⬡──⬡
-📫Pengirim : Someone
+╭━━━━「 *PESAN* 」
+📝 ${txt}
 
-💌 Pesan : ${pesan}
-⬡──⬡─────────⬡──⬡
-Maaf Anda Belum Bisa Membalas ke Pengirim
 
-------------------------------------------
 
-▮PENGIRIM RAHASIA 」 
-Anda Ingin Mengirimkan Pesan ke pacar/sahabat/teman/doi/
-mantan?, tapi Tidak ingin tau siapa Pengirimnya?
-Kamu bisa menggunakan Bot ini
-Contoh Penggunaan: .menfess nomor|pesan untuknya
-Contoh: .menfess 628xxxxxxxxxx|hai owner`
 
-    conn.reply(korban + '@s.whatsapp.net', spam1, m)
-
-    let logs = `Sukses Mengirim Pesan
-👥 Dari : wa.me/${nomor.split("@s.whatsapp.net")[0]}
-⬡──⬡─────────⬡──⬡
-💌 Isi Pesan : ${pesan}
-⬡──⬡─────────⬡──⬡`
-
-    conn.reply(m.chat, logs, m)
+┬ 
+│
+╰━━━━━━━━━━━━━━━━┈─◂
+     ▌│█║▌║▌║║▌║▌║█│▌
+     
+     
+${botdate}
+`
+	let tujuan = `${htki} MENFES ${htka}`
+	if (!m.quoted) {
+		await conn.sendHydrated(mention, tujuan, cap, thumbmenfes, 'https://wa.me/62895394988123?text=.menfes', '💬 Balas pesan', null, null, [
+			[null, null]
+		], null)
+	} else {
+		await conn.sendHydrated(mention, tujuan, cap, thumbmenfes, 'https://wa.me/62895394988123?text=.menfes', '💬 Balas pesan', null, null, [
+			[null, null]
+		], null)
+		let media = q ? await m.getQuotedObj() : false || m
+		await conn.copyNForward(mention, media, true).catch(_ => _)
+	}
+	let suks = `Berhasil Mengirim Pesan *${mime ? mime : 'Teks'}*`
+	await conn.reply(pengirim, suks, m)
 }
-handler.help = ['menfess2 nomor|pesan']
-handler.tags = ['menbalas']
+handler.help = ['menfess <pesan>']
+handler.tags = ['premium']
 
-handler.command = /^(menfess2|confess2|menfes2|confes2)$/i
-
-handler.limit = true
 handler.private = true
+handler.premium = true
+
+handler.command = /^(men(fess?|cret)|chat)$/i
 export default handler
