0d406e77df0f9ec59b1e98a86291f09b937a1f78
BOTz44 <106071317+BOTz44@users.noreply.github.com>
2022-10-17 10:07:50 +0700

    Update menfess.js

--- plugins/menfess.js
+++ plugins/menfess.js
@@ -1,44 +1,44 @@
-// Ambil Aja Gak Papa
+import fetch from 'node-fetch'
+let handler = async(m, {
+	conn, text, usedPrefix, command, args
+}) => {
 
-let anonymous = {}
-
-function createRoom(a, b) {
-    let room = Object.values(anonymous).find(p => p.check(a))
-    if (!!room) return !1
-    let id = Date.now()
-    anonymous[id] = {
-        id: id,
-        a: a,
-        b: b,
-        state: "CHATTING",
-        check: function(p) {
-            return [this.a, this.b].includes(p)
-        },
-        other: function(p) {
-            return p == this.a ? this.b : p == this.b ? this.a : ""
-        }
-    }
-    return Object.values(anonymous).find(p => p.check(a))
+	if (args[0].length > 14) throw 'Nomor Kepanjangan'
+	if (args[0].length < 7) throw 'Nomor Kependekan'
+	if (args[0].startsWith('0')) throw 'Gunakan format 62'
+	if (!args[0]) throw 'Masukkan Teks'
+	
+    let mention = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : args[0] ? (args[0].replace(/[@ .+-]/g, '') + '@s.whatsapp.net') : ''
+	let txt = (args.length > 1 ? args.slice(1).join(' ') : '') || ''
+	let q = m.quoted ? m.quoted : m
+	let mime = (q.msg || q).mimetype || ''
+	let tujuan = `👋 Saya *${conn.user.name}*, Pesan Untuk Kamu
+👥 Dari : *PENGIRIM RAHASIA*
+${htki} 💌 Pesan ${htka}
+${htjava} ${txt}
+`
+	let cap = `${htki} PESAN RAHASIA ${htka}
+Anda Ingin Mengirimkan Pesan ke pacar/sahabat/teman/doi/
+mantan?, tapi Tidak ingin tau siapa Pengirimnya?
+Kamu bisa menggunakan Bot ini
+Contoh Penggunaan: ${usedPrefix + command} ${nomorown} pesan untuknya
+Contoh: ${usedPrefix + command} ${nomorown} hai`
+	if (!m.quoted) {
+		await conn.sendButton(mention, tujuan, cap, null, [['Menu', '/menu']], m)
+	} else {
+		await conn.sendButton(mention, tujuan, cap, null, [['Menu', '/menu']], m)
+		let media = q ? await m.getQuotedObj() : false || m
+		await conn.copyNForward(mention, media, false).catch(_ => _)
+	}
+	let suks = `Mengirim Pesan *${mime ? mime : 'Teks'}*
+👥 Dari : @${m.sender.replace(/@.+/, '')}
+👥 Untuk : @${mention.replace(/@.+/, '')}
+${htki} 💌 Pesan ${htka}
+${htjava} ${txt}
+`
+	await conn.sendButton(m.chat, suks, wm, null, [['Menu', '/menu']], m, { mentions: conn.parseMention(suks) })
 }
-
-function leaveRoom(ab) {
-    let room = Object.values(anonymous).find(p => p.check(ab))
-    if (!room) return !1
-    let other = room.other(ab)
-    delete anonymous[room.id]
-    return other
-}
-handler.tags = ['premium']
-handler.owner = benar
-handler.mods = false
-handler.premium = true
-handler.group = false
-handler.private = false
-
-handler.admin = salah
-handler.botAdmin = false
-
-handler.fail = null
-handler.limit = true
-
+handler.help = ['menfess <pesan>']
+handler.tags = ['menbalas']
+handler.command = /^(menfess|confess|menfes|confes)$/i
 export default handler
