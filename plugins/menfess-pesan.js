04427dfd8d34db73ec3c67754183c8b943aecbc1
BOTz44 <106071317+BOTz44@users.noreply.github.com>
2022-10-18 00:19:06 +0700

    Update menfess-pesan.js

--- plugins/menfess-pesan.js
+++ plugins/menfess-pesan.js
@@ -39,11 +39,10 @@ Contoh: .menfess 628xxxxxxxxxx|hai owner`
     conn.reply(m.chat, logs, m)
 }
 handler.help = ['menfess2 nomor|pesan']
-handler.tags = ['premium'menbalas']
+handler.tags = ['menbalas']
 
 handler.command = /^(menfess2|confess2|menfes2|confes2)$/i
 
 handler.limit = true
-handler.premium = true
 handler.private = true
 export default handler
